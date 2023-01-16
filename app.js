8c991b45963efeae970e5a452b06e0b316d4e383
sureshsahu <suresh.sahu@trangile.com>
2023-01-16 15:21:27 +0530

    export app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express=require("express");
-const app=express();
-app.get("/",(req,res)=>{
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
     res.send("hi all")
 })
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("app running at 8080")
 })
+module.exports = app;
